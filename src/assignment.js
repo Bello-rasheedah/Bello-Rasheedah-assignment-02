72950b57826b6470f84fda525f23785758f08427
Rasheedah <109388510+Bello-rasheedah@users.noreply.github.com>
2022-07-15 20:35:45 +0000

    Final submission

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+  arrayOfNumbers.forEach((num) => {
+    sum += num;
+    return sum;
+  });
+
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +34,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let countEven = 0;
+  arrayOfNumbers.forEach((num) => {
+    if (num % 2 == 0) {
+      countEven++;
+    }
+  });
+  return countEven;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
